3188f27bf78b59d6b9ef30ca7665cbb1297e0f2a
RIRU <riruoda@gmail.com>
2023-05-10 22:44:02 +0900

    MFT EntryとMFT Attributeの解析処理を実装

--- ntfsdump.c
+++ ntfsdump.c
@@ -8,11 +8,104 @@
 
 #include "ntfsdump.h"
 
+// update sequenceを比較して、一致していたら元の値を書き戻す。(とりあえず今は一致するか確認するだけ)
+static void cmpAndRestore(uint16_t *fixup, uint16_t *end) {
+    if(*fixup != *(end -1)) {
+        printf(
+            "\tcmp %.4x %.4x\n",
+            *fixup,
+            *(end -1)
+        );
+    }
+
+    printf(
+        "\tcmp %.4x %.4x\n",
+        *fixup,
+        *(end -1)
+    );
+    
+    printf(
+        "\toverwirte %.4x with %.4x\n",
+        *(end - 1),
+        *(fixup + 1)
+    );
+}
+
+// MFTEntryFlagを文字列に変換
+static char * printMFTEntryFlags(uint16_t flag) {
+    switch(flag) {
+        case MFT_RECORD_IN_USE:
+            return "MFT_RECORD_IN_USE";
+        case MFT_RECORD_IS_DIRECTORY:
+            return "MFT_RECORD_IS_DIRECTORY";
+        case MFT_RECORD_IN_EXTEND:
+            return "MFT_RECORD_IN_EXTEND";
+        case MFT_RECORD_IS_VIEW_INDEX:
+            return "MFT_RECORD_IS_VIEW_INDEX";
+        default:
+            return "";
+    }
+}
+
+// AttributeTypeを文字列に変換
+static char * printAttributeType(uint32_t ty) {
+    switch(ty) {
+        case STANDARD_INFORMATION:
+            return "STANDARD_INFORMATION";
+        case ATTRIBUTE_LIST:
+            return "ATTRIBUTE_LIST";
+        case FILE_NAME:
+            return "FILE_NAME";
+        case OBJECT_ID:
+            return "OBJECT_ID";
+        case SECURITY_DESCRIPTOR:
+            return "SECURITY_DESCRIPTOR";
+        case VOLUME_NAME:
+            return "VOLUME_NAME";
+        case VOLUME_INFORMATION:
+            return "VOLUME_INFORMATION";
+        case DATA:
+            return "DATA";
+        case INDEX_ROOT:
+            return "INDEX_ROOT";
+        case INDEX_ALLOCATION:
+            return "INDEX_ALLOCATION";
+        case BITMAP:
+            return "BITMAP";
+        case REPARSE_POINT:
+            return "REPARSE_POINT";
+        case EA_INFORMATION:
+            return "EA_INFORMATION";
+        case EA:
+            return "EA";
+        case LOGGED_UTILITY_STREAM:
+            return "LOGGED_UTILITY_STREAM";
+        case END_OF_ATTRIBUTE:
+            return "END_OF_ATTRIBUTE";
+        default:
+            return "";
+    }
+}
+
+// NameSpaceを文字列に変換
+static char * printNameSpace(uint8_t v) {
+    switch(v) {
+        case POSIX:
+            return "POSIX";
+        case WINDOWS:
+            return "WINDOWS";
+        case DOS:
+            return "DOS";
+        case DOS_WINDOWS:
+            return "DOS_WINDOS";
+        default:
+            return "";
+    }
+}
+
 int main(int argc, char *argv[]) { 
     int fd;
     struct stat sb;
-    uint8_t *base;
-    VolumeHeader *hdr;
 
     if(argc != 2) {
         puts("Usage: ./ntfsdump <path>");
@@ -29,6 +122,11 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    uint8_t *base;
+    VolumeHeader *vhdr;
+    uint64_t clusterSize;
+    uint64_t offset = 0;
+
     base = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
     if(base == MAP_FAILED) {
@@ -36,19 +134,112 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    hdr = (VolumeHeader *)base;
-
-    printf("BytesPerSector\tTotalNumberOfSectors\tSectorsPerCluster\tMFTCBN\tMFTEntrySize\n");
+    vhdr = (VolumeHeader *)base;
+    clusterSize = vhdr->BytesPerSector * sectors_per_cluster_block(vhdr->SectorsPerClusterBlock);
+    printf("VolumeHeader(+%#lx)\n", offset);
     printf(
-        "%#x\t\t%#lx\t\t\t%#x\t\t\t%#lx\t%#x\n",
-        hdr->BytesPerSector,
-        hdr->TotalNumberOfSectors,
-        sectors_per_cluster_block(hdr->SectorsPerClusterBlock),
-        hdr->MFTClusterBlockNumber,
-        entry_size(hdr->MFTEntrySize)
+        "BytesPerSector: %#x\nTotalNumberOfSectors: %#lx\nSectorsPerCluster: %#x\nMFTCBN: %#lx\nMFTEntrySize: %#x\n\n",
+        vhdr->BytesPerSector,
+        vhdr->TotalNumberOfSectors,
+        sectors_per_cluster_block(vhdr->SectorsPerClusterBlock),
+        vhdr->MFTClusterBlockNumber,
+        entry_size(vhdr->MFTEntrySize)
     );
 
+    // read MFT
+    offset = clusterSize * vhdr->MFTClusterBlockNumber;
+    MFTEntryHeader *enthdr = (MFTEntryHeader *)(base + offset);
+
+    printf("MFT Entry(+%lx)\n", offset);
+    printf(
+        "Signature: %.4s\n",
+        enthdr->Signature
+    );
+    puts("FixupValue:");
+    printf(
+        "\toffset: %#x\n\tnum: %#x\n",
+        enthdr->FixupValueOffset,
+        enthdr->NumberOfFixupValues
+    );
+
+    cmpAndRestore(
+        (uint16_t *)((uint8_t *)enthdr + enthdr->FixupValueOffset),
+        (uint16_t *)((uint8_t *)enthdr + enthdr->TotalEntrySize)
+    );
+    
+    printf(
+        "AttributeOffset: %#x\nUsedEntrySize: %#x\nTotalEntrySize: %#x\n",
+        enthdr->AttributeOffset,
+        enthdr->UsedEntrySize,
+        enthdr->TotalEntrySize
+    );
+
+    printf(
+        "EntryFlags: %#x(%s)\n\n",
+        enthdr->EntryFlags,
+        printMFTEntryFlags(enthdr->EntryFlags)
+    );
+
+    // read MFTAttributes
+    offset += enthdr->AttributeOffset;
+    MFTAttributeHeader *attrhdr = (MFTAttributeHeader *)(base + offset);
+    NonResidentMFTAttribute *nonResidentAttr;
+    ResidentMFTAttribute *regidentAttr;
+    FileNameAttribute *fnameAttr;
+
+    printf("MFT Attributes(+%#lx)\n", offset);
+
+    while(attrhdr->AttributeType != END_OF_ATTRIBUTE) {
+        printf("+%#lx\n", offset);
+        printf(
+            "type: %#x(%s)\nSize: %#x\nNon-resident: %#x\n",
+            attrhdr->AttributeType,
+            printAttributeType(attrhdr->AttributeType),
+            attrhdr->Size,
+            attrhdr->NonRegidentFlag
+        );
+
+        if(attrhdr->NonRegidentFlag) {
+            // Non-residentだった場合
+            nonResidentAttr = (NonResidentMFTAttribute *)((uint8_t *)attrhdr + sizeof(MFTAttributeHeader));
+            printf(
+                "Data Runs Offset: %#x\nDataSize: %#lx\n",
+                nonResidentAttr->DataRunsOffset,
+                nonResidentAttr->DataSize
+            );
+        } else {
+            // Residentだった場合
+            regidentAttr = (ResidentMFTAttribute *)((uint8_t *)attrhdr + sizeof(MFTAttributeHeader));
+            printf(
+                "DataSize: %#x\nData Offset: %#x\n",
+                regidentAttr->DataSize,
+                regidentAttr->DataOffset
+            );
+            // FileName属性がNon-residentになることはあるのか？
+            if(attrhdr->AttributeType == FILE_NAME) {
+                fnameAttr = (FileNameAttribute *)((uint8_t *)attrhdr + regidentAttr->DataOffset);
+            }
+        }
+
+        putchar('\n');
+
+        offset += attrhdr->Size;
+        attrhdr = (MFTAttributeHeader *)(base + offset);
+    }
+
+    // ファイル名を表示
+    printf(
+        "NameSpace: %#x(%s)\nFileName : ",
+        fnameAttr->Namespace,
+        printNameSpace(fnameAttr->Namespace)
+    );
+    for(int i = 0; i < fnameAttr->NameStringSize; i++) {
+        printf("%c", fnameAttr->Name[i * 2]);
+    }
+    putchar('\n');
+
     munmap((void *)base, sb.st_size);
+    close(fd);
 
     return 0;
 }
--- ntfsdump.h
+++ ntfsdump.h
@@ -10,7 +10,7 @@
 #define Unknown LABEL1(__LINE__)
 #define Unused  LABEL2(__LINE__)
 
-//　VolumeHeader is defined on ch4
+// VolumeHeader is defined on 4
 typedef struct __attribute__((packed)) {
     uint8_t     Entry[3];
     uint8_t     OEM[8];
@@ -45,5 +45,117 @@ typedef struct __attribute__((packed)) {
 
 #define entry_size(s) (128 <= (s) ? 1 << (256 - (s)) : (s))
 #define sectors_per_cluster_block(s) (244 <= (s) ? 1<<(256 - (s)) : (s))
+
+// MFTEntryHeader dedined on 5
+typedef struct __attribute__((packed)) {
+    uint64_t    MFTEntryIndex   : 48;
+    uint16_t    SequenceNumber  : 16;
+} FileReference;
+
+typedef struct __attribute__((packed)) {
+    uint8_t         Signature[4]; 
+    uint16_t        FixupValueOffset;
+    uint16_t        NumberOfFixupValues;
+    uint64_t        LSNOfLogfile;
+    uint16_t        Sequence;
+    uint16_t        RefCount;
+    uint16_t        AttributeOffset;
+    uint16_t        EntryFlags;
+    uint32_t        UsedEntrySize;
+    uint32_t        TotalEntrySize;
+    FileReference   BaseRecordFileRefs;
+    uint16_t        FirstAvailableAttributeIndentifer;
+    uint16_t        Unknown;
+    uint32_t        Unknown;
+} MFTEntryHeader;
+
+typedef enum {
+    MFT_RECORD_IN_USE           = 1,
+    MFT_RECORD_IS_DIRECTORY     = 1<<2,
+    MFT_RECORD_IN_EXTEND        = 1<<3,
+    MFT_RECORD_IS_VIEW_INDEX    = 1<<4
+} MFTEntryflags;
+
+// MFTAttributeHeader defined on 5.5.1
+typedef struct __attribute__((packed)) {
+    uint32_t    AttributeType;
+    uint32_t    Size;
+    uint8_t     NonRegidentFlag; // 0 = RESIDENT_FORM, 1 = NONRESIDENT_FORM
+    uint8_t     NameSize;
+    uint16_t    NameOffset;
+    uint16_t    AttributeDataFlags;
+    uint16_t    AttributeIdentifier; // or Instance
+} MFTAttributeHeader;
+
+// AttributeTypes defined on 6.1
+typedef enum {
+    STANDARD_INFORMATION    =   0x00000010,
+    ATTRIBUTE_LIST          =   0x00000020,
+    FILE_NAME               =   0x00000030,
+    OBJECT_ID               =   0x00000040, 
+    SECURITY_DESCRIPTOR     =   0x00000050,
+    VOLUME_NAME             =   0x00000060,
+    VOLUME_INFORMATION      =   0x00000070,
+    DATA                    =   0x00000080,
+    INDEX_ROOT              =   0x00000090,
+    INDEX_ALLOCATION        =   0x000000a0,
+    BITMAP                  =   0x000000b0,
+    REPARSE_POINT           =   0x000000c0,
+    EA_INFORMATION          =   0x000000d0,
+    EA                      =   0x000000e0,
+    LOGGED_UTILITY_STREAM   =   0x00000100,
+    END_OF_ATTRIBUTE        =   0xffffffff,
+} AttributeTypes;
+
+typedef enum {
+    ATTRIBUTE_FLAG_COMPRESSION_MASK = 0x00ff,
+    ATTRIBUTE_FLAG_ENCRYPTED        = 0x4000,
+    ATTRIBUTE_FLAG_SPARSE           = 0x8000
+} MFTAttributeDataFlags;
+
+// ResidentMFTAttribute defined on 5.5.2
+typedef struct __attribute__((packed)) {
+    uint32_t    DataSize;
+    uint16_t    DataOffset; // from MFTAttribute
+    uint8_t     IndexedFlag;
+    uint8_t     Padding;
+} ResidentMFTAttribute;
+
+// NonResidentMFTAttribute defined on 5.5.3
+typedef struct __attribute__((packed)) {
+    uint64_t    FirstVCNOfData;
+    uint64_t    LastVCNOfData;
+    uint16_t    DataRunsOffset;
+    uint16_t    CompressionUnitSize;
+    uint32_t    Padding;
+    uint64_t    AllocatedDataSize;
+    uint64_t    DataSize;
+    uint64_t    ValidDataSize;
+    // uint64_t    TotalAllocatedSize; used if CompressionUnitSize > 0
+} NonResidentMFTAttribute;
+
+// FileNameAttribute defined on 6.4
+typedef struct __attribute__((packed)) {
+    uint64_t    ParentFileRef;
+    uint64_t    CTime; // creation data and time
+    uint64_t    WTime; // last written data and time
+    uint64_t    MTime; // last modification data and time
+    uint64_t    ATime; // last access data and time
+    uint64_t    AllocatedFileSize;
+    uint64_t    FileSize;
+    uint32_t    FileAttributeFlags;
+    uint32_t    ExtendedData;
+    uint8_t     NameStringSize;
+    uint8_t     Namespace;
+    uint8_t     Name[];
+} FileNameAttribute;
+
+// NameSpace defined on 6.4.1
+typedef enum {
+    POSIX,
+    WINDOWS,
+    DOS,
+    DOS_WINDOWS
+} NameSpace;
 
 #endif 
